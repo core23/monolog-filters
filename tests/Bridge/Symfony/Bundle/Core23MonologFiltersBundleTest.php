2dbc10486ac62fb406648ca6e76745219e012dfd
core23 <mail@core23.de>
2019-03-18 22:14:03 +0100

    [BUILD] Added more tests

---
+++ tests/Bridge/Symfony/Bundle/Core23MonologFiltersBundleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\MonologFilters\Tests\Bridge\Symfony\Bundle;
+
+use Core23\MonologFilters\Bridge\Symfony\Bundle\Core23MonologFiltersBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+class Core23MonologFiltersBundleTest extends TestCase
+{
+    public function testItIsInstantiable(): void
+    {
+        $bundle = new Core23MonologFiltersBundle();
+
+        $this->assertInstanceOf(BundleInterface::class, $bundle);
+    }
+
+    public function testGetPath(): void
+    {
+        $bundle = new Core23MonologFiltersBundle();
+
+        $this->assertStringEndsWith('Bridge/Symfony/Bundle', \dirname($bundle->getPath()));
+    }
+}
